a39f0009b4f67ddc15a4745164606b70826f76a8
kaoxiong20 <kaoxiong520@gmail.com>
2023-04-19 18:07:51 -0500

    Added an unordered list of data from the dogFacts array and set state to that; each i tem is getting a list item

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Fetch API Demo
+
+Find public APIs
+https://github.com/public-apis/public-apis
+https://kinduff.github.io/dog-api/
--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  
+
+  const [dogFacts, setDogFacts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dogapi.dog/api/facts?number=5')
+    .then(response => response.json())
+    .then(data => setDogFacts(data.facts))
+    .catch(error => console.log(error));
+    console.log(dogFacts);
+  }, [])
+
   return (
     <div className="container">
       <div className="content">
         <h1 className="title">Random Dog Facts</h1>
         <img src="./imgs/dog.jpg"></img>
-
+        <ul className='list'></ul>
+          {dogFacts.map((fact, index) => (
+            <li className='item' key={index}>
+              {fact}
+            </li>
+          ))}  
       </div>
     </div>
   );
