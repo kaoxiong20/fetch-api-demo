25c040eb59d8b4bf2f111358f7d79a96d7f45436
Mase <48336140+MaseSantos@users.noreply.github.com>
2023-04-19 17:06:00 -0500

    Changed README

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-
+  
   return (
     <div className="container">
       <div className="content">
         <h1 className="title">Random Dog Facts</h1>
         <img src="./imgs/dog.jpg"></img>
-        
+
       </div>
     </div>
   );
