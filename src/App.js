f5be7ade4b92cbea78c3ba6a5d07964a64720500
Mase <48336140+MaseSantos@users.noreply.github.com>
2023-04-19 17:03:12 -0500

    Code for Demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="content">
+        <h1 className="title">Random Dog Facts</h1>
+        <img src="./imgs/dog.jpg"></img>
+        
+      </div>
     </div>
   );
 }
